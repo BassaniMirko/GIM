63cb4b6c08d1b1e8b1e4658db87a058dec533659
BassaniMirko <167759821+BassaniMirko@users.noreply.github.com>
2024-05-27 11:39:52 +0200

    Update sketch.js

--- Esercizio_3B/pong copia/sketch.js
+++ Esercizio_3B/pong copia/sketch.js
@@ -1,53 +1,63 @@
-var pongText = 'Pong';
-var pongPoints = [];
-var vehicles = [];
-var ball = { x: 0, y: 0 }; // Posizione iniziale della palla
+var pongText = 'Pong'
+var pongPoints = []
+var vehicles = []
+var ball = { x: 0, y: 0 } // Posizione iniziale della palla
 var maxChangeForce = 5; // Forza massima desiderata
 var font;
 
 // Caricamento del font
 function preload() {
-    font = loadFont('assets/Helvetica.otf'); // Assicurati di avere il font nel percorso corretto
+    font = loadFont('assets/Helvetica.otf') // Assicurati di avere il font nel percorso corretto
 }
 
 // Definizione della classe Vehicle
 function Vehicle(x, y) {
-    this.pos = createVector(x, y);
-    this.vel = createVector();
-    this.acc = createVector();
+    this.pos = createVector(x, y)
+    this.vel = createVector()
+    this.acc = createVector()
+    this.target = createVector(x, y)
 }
 
 Vehicle.prototype.applyForce = function (force) {
-    this.acc.add(force);
+    this.acc.add(force)
 }
 
 Vehicle.prototype.behaviors = function () {
-    // Implementa i comportamenti desiderati per il veicolo
+    var seek = this.seek(this.target)
+    this.applyForce(seek)
+}
+
+Vehicle.prototype.seek = function(target) {
+    var desired = p5.Vector.sub(target, this.pos)
+    desired.setMag(0.3)
+    var steer = p5.Vector.sub(desired, this.vel)
+    steer.limit(0.1)
+    return steer
 }
 
 Vehicle.prototype.update = function () {
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
+    this.vel.add(this.acc)
+    this.pos.add(this.vel)
     this.acc.mult(0); // Reset dell'accelerazione
 }
 
 Vehicle.prototype.show = function () {
     stroke(255);
     strokeWeight(8);
-    point(this.pos.x, this.pos.y);
+    point(this.pos.x, this.pos.y)
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight); // Correzione di windowWidth e windowHeight
 
     // Creare i punti per la parola 'Pong'
-    var bounds = font.textBounds(pongText, 0, 0, 192);
-    var posx = width / 2 - bounds.w / 2;
-    var posy = height / 2 + bounds.h / 2;
+    var bounds = font.textBounds(pongText, 0, 0, 192)
+    var posx = width / 2 - bounds.w / 2
+    var posy = height / 2 + bounds.h / 2
 
     pongPoints = font.textToPoints(pongText, posx, posy, 192, {
         sampleFactor: 0.1
-    });
+    })
 
     for (var i = 0; i < pongPoints.length; i++) {
         var pt = pongPoints[i];
